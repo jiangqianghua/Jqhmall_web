f2e4bde3ab5496ec7e4ad9d4b3b1da52af70806e
jiangqianghua <240437339@qq.cvom>
2018-10-21 17:59:25 +0800

    添加商品列表

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ var config ={
 		'user-pass-update':['./src/page/user-pass-update/index.js'],
 		'user-center':['./src/page/user-center/index.js'],
 		'user-center-update':['./src/page/user-center-update/index.js'],
+		'list':['./src/page/list/index.js'],
 		'common':['./src/page/common/index.js'],  // 该模块回放在base.js中
 		'result':['./src/page/result/index.js'],
 	},
@@ -119,7 +120,8 @@ var config ={
 		new HtmlWebpackPlugin(getHtmlConfig('user-center','个人中心')),
 		new HtmlWebpackPlugin(getHtmlConfig('user-center-update','修改个人信息')),
 		new HtmlWebpackPlugin(getHtmlConfig('user-pass-update','修改密码')),
-		new HtmlWebpackPlugin(getHtmlConfig('result','结果页面'))
+		new HtmlWebpackPlugin(getHtmlConfig('result','结果页面')),
+		new HtmlWebpackPlugin(getHtmlConfig('list','商品列表'))
 	]
 }
 
